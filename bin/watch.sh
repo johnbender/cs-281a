01d61efa4e2c506010070edd7db56bacb1e03f16
John Bender <john.m.bender@gmail.com>
2014-01-07 15:41:53 -0800

    initial commit on scribe notes for lecture 1 of 281a, computational complexity at UCLA Winter 2014

---
+++ bin/watch.sh
@@ -0,0 +1,14 @@
+build(){
+	pdflatex -interaction=nonstopmode $1
+	bibtex $2
+	pdflatex -interaction=nonstopmode $1
+	pdflatex -interaction=nonstopmode $1
+}
+
+# build once by default
+build $1 $2
+
+# watch for alterations
+while inotifywait *.tex *.bib; do
+  build $1 $2
+done
